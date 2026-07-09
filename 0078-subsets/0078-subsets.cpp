8a7d42ac64f48176c213963bfc0d89e258b96261
Shubham Sharma <147817185+itzzshubhamm@users.noreply.github.com>
2026-07-09 23:18:58 +0530

    Time: 5 ms (3.69%), Space: 15.9 MB (5.53%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+     vector<vector<int>> answer;
+   
+     void subsetsCreator(vector<int>& nums , vector<int> curr , int index){
+
+        if(index == nums.size()){
+            answer.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[index]);
+        subsetsCreator(nums , curr , index+1);
+        curr.pop_back();
+        subsetsCreator(nums , curr , index+1);
+     }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<int> curr;
+        subsetsCreator(nums , curr , 0);
+
+        return answer;
+        
+    }
+};

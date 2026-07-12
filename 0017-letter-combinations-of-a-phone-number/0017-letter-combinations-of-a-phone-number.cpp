430413294eb6933ff04693e9507f55a553dd199b
Shubham Sharma <147817185+itzzshubhamm@users.noreply.github.com>
2026-07-12 23:10:44 +0530

    Time: 2 ms (16.21%), Space: 9.4 MB (45.42%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> answerToReturn;
+    vector<string> keypad = {
+    "",      // 0
+    "",      // 1
+    "abc",   // 2
+    "def",   // 3
+    "ghi",   // 4
+    "jkl",   // 5
+    "mno",   // 6
+    "pqrs",  // 7
+    "tuv",   // 8
+    "wxyz"   // 9
+    };
+
+    void letterCombinationHelper(string& digits , string& curr , int index){
+
+        if(index == digits.size()){
+            answerToReturn.push_back(curr);
+            return;
+        }  
+
+        int indexForKey = digits[index] - '0';
+        string mappedString = keypad[indexForKey];
+
+        for(char ch : mappedString){     
+            curr += ch;
+            letterCombinationHelper(digits, curr, index+1);
+            curr.pop_back();
+        }
+
+    }
+
+    vector<string> letterCombinations(string digits) {
+
+    string current;
+    letterCombinationHelper(digits, current, 0);
+
+    return answerToReturn;
+        
+    }
+};

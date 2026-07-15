9cf09c2b435ad58e2683a7df8c9ef54186fa0d2b
Shubham Sharma <147817185+itzzshubhamm@users.noreply.github.com>
2026-07-15 23:11:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (57.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 
+// Raw recurssion method
     // int climbStairs(int n) {
     //     if(n<=1){
     //         return 1;
@@ -9,25 +10,50 @@ public:
         
     // }
 
-   int solve(int n , vector<int>& dp){
+
+    // DP Memoization method
+
+//    int solve(int n , vector<int>& dp){
+
+//        if(n<=1){
+//             return 1;
+//         }
+
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+
+//         return dp[n] = solve(n-2 , dp) + solve(n-1 , dp);
+
+//    }
+   
+
+   int solve(int n){
 
        if(n<=1){
             return 1;
         }
 
-        if(dp[n] != -1){
-            return dp[n];
+        int prev1=1;
+        int prev2=1;
+        int current;
+
+        for(int i=2 ; i<=n ; i++){
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;   
         }
 
-        return dp[n] = solve(n-2 , dp) + solve(n-1 , dp);
+
+       return current;
 
    }
 
 
     
     int climbStairs(int n) {
-        vector<int> dp(n+1 , -1);
-        return solve(n,dp);
+     
+        return solve(n);
         
     }
 

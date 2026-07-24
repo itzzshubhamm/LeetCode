a5e9057cb69259b761591c9dd2695f6e828a3327
Shubham Sharma <147817185+itzzshubhamm@users.noreply.github.com>
2026-07-24 09:54:44 +0530

    Update 0062-unique-paths.cpp

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int solve( int m , int n, vector<vector<int>>& dp ){
-
-        for(int i =0 ; i < m ; i++){
-            dp[i][n-1] = 1; 
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            dp[m-1][i] = 1; 
-        }
-            
-  
-        for(int i=m-2 ; i >= 0 ;i--){
-            for(int j=n-2 ; j>= 0 ; j--){
-                dp[i][j] = dp[i+1][j] + dp[i][j+1]; 
-            }
-        }
-
-
-        return dp[0][0];
-    }
-
-    int uniquePaths(int m, int n) {
-
-        vector<vector<int>> dp( m +1 , vector<int> (n +1 , 0));
-
-        return solve(m, n, dp);
-        
-    }
+    int solve( int m , int n ){
+ 
+       
+        vector<int> next(n ,1);
+ 
+        for(int i=m-2; i >= 0; i--){
+            vector<int> curr ( n , 0);
+            curr[n-1]=1;
+            for(int j=n-2 ; j>= 0 ; j--){
+                curr[j] = next[j] + curr[j+1];
+            }
+            next = curr;
+        }
+ 
+ 
+        return next[0];
+    }
+ 
+    int uniquePaths(int m, int n) {
+ 
+        return solve(m, n);
+       
+    }
 };

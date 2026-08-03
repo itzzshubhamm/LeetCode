9a6474be7e4d339b090ce862eddd3ec1d0741ca8
Shubham Sharma <147817185+itzzshubhamm@users.noreply.github.com>
2026-08-03 23:09:29 +0530

    Time: 0 ms (100%), Space: 52.3 MB (58.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,9 +3,9 @@ public:
     bool canJump(vector<int>& nums) {
 
         int farthest=0;
-        int n = nums.size();
+        // int n = nums.size();
 
-        for(int i=0 ; i < n ; i++){
+        for(int i=0 ; i < nums.size() ; i++){
             if(i > farthest){
                 return false;
             }

6b67ad1f7f73db4e262725ce5d0aec072b9ab902
Shubham Sharma <147817185+itzzshubhamm@users.noreply.github.com>
2026-08-03 23:09:58 +0530

    Time: 0 ms (100%), Space: 52.3 MB (58.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,7 +5,7 @@ public:
         int farthest=0;
         int n = nums.size();
 
-        for(int i=0 ; i < nums.size() ; i++){
+        for(int i=0 ; i < n ; i++){
             if(i > farthest){
                 return false;
             }
